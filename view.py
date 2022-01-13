232fa909d91dc7442b42aa562ac4567c8b722231
shubham <shubham@tp-server11.sm.sony.co.jp>
2022-01-13 20:28:58 +0900

    api for git pages

---
+++ view.py
@@ -0,0 +1,11 @@
+
+import http.server
+import socketserver
+
+PORT = 9036
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
